a348a59f9c2adb60a730b1627c4757dbdfd4cfac
Vaishno Chaitanya G <vaishnochaitanya7@gmail.com>
2021-05-18 19:51:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ alive = int(os.getenv('tokentimetolive', 60))
 
 
 # # start the app with port 5000 and debug on!
-# if __name__ == '__main__':
-#     vuln_app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    vuln_app.run()
